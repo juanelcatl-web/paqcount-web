2671486a6aa1cf20837916a1d216b12718292740
juanelcatl-web <juanelcatl@gmail.com>
2026-03-21 16:57:18 -0300

    feat: PaqCount MVP - escaneo, entrada/salida, resumen, duplicados

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Service Worker desactivado temporalmente
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
